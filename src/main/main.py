643d4d4f4a51b134e149e68b47249729ac6f3716
Hihthei <cbouillon@et.esiea.fr>
2024-07-23 16:38:01 +0200

    main

--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,8 @@
 import json
-import os
 from pathlib import Path
 from src.forma_fun.forma_func import list_main_ui
 
-chemin_json = Path(__file__).resolve().parents[3] / 'data_list' / 'grocery_list' / 'grocery_data.json'
+chemin_json = Path(__file__).resolve().parents[2] / 'data_list' / 'grocery_list' / 'grocery_data.json'
 
 grocery_list = None
 
@@ -11,7 +10,7 @@ if not chemin_json.exists():
     print("fichier inexistant")
 
     with open(chemin_json, 'w') as file:
-        json.dump("", file, indent=4)
+        json.dump([], file, indent=4)
 else:
     grocery_list = json.load(open(chemin_json))
 
