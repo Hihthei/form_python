2346df997e6fd2650d151e1c914b15cb5d1ce42c
Hihthei <cbouillon@et.esiea.fr>
2024-07-23 12:53:43 +0200

    exercice liste de course

--- src/forma_fun/forma_func.py
+++ src/forma_fun/forma_func.py
@@ -22,7 +22,8 @@ def list_append(grocery_list: list = None, grocery_name: str = None) -> list or 
     current_len = len(grocery_list)
 
     tmp_list = grocery_name.split(" ")
-    grocery_list.extend(element.lower().title() for element in tmp_list if element not in grocery_list and element.isalpha())
+    grocery_list.extend(
+        element.lower().title() for element in tmp_list if element not in grocery_list and element.isalpha())
 
     if len(grocery_list) > current_len:
         elements_str = " ".join(f"{element.lower().title()}" for element in tmp_list if element.isalpha())
@@ -60,3 +61,32 @@ def list_empty(grocery_list: list = None) -> None:
 
     if len(grocery_list) == 0:
         print("La liste a été vidé avec succès !")
+
+
+def list_main_ui(grocery_list: list = None) -> list:
+    while True:
+        print("Choisissez parmi les 5 options suivantes :\n"
+              "1_ Ajouter un ou plusieurs éléments à la liste\n"
+              "2_ Retirer un ou plusieurs éléments à la liste\n"
+              "3_ Afficher la liste\n"
+              "4_ Vider la liste\n"
+              "5_ Quitter le programme\n")
+
+        user_choice = input("Veuillez saisir votre réponse : ")
+
+        if user_choice == "1":
+            grocery_list = list_append(grocery_list, input("Quel(s) ingrédient(s) souhaitez-vous ajouter ?\n"))
+        elif user_choice == "2":
+            list_remove(grocery_list, input("Quel(s) ingrédient(s) souhaitez-vous retirer ?\n"))
+        elif user_choice == "3":
+            list_print(grocery_list)
+        elif user_choice == "4":
+            list_empty(grocery_list)
+        elif user_choice == "5":
+            break
+        else:
+            continue
+
+        print("\n")
+
+    return grocery_list
--- src/main/main.py
+++ src/main/main.py
@@ -1,31 +1,21 @@
+import json
+import os
+from pathlib import Path
+from src.forma_fun.forma_func import list_main_ui
 
-# exercice liste de course
+chemin_json = Path(__file__).resolve().parents[3] / 'data_list' / 'grocery_list' / 'grocery_data.json'
 
-from src.forma_fun.forma_func import list_append, list_remove, list_print, list_empty
+grocery_list = None
 
-grocery_list = []
+if not chemin_json.exists():
+    print("fichier inexistant")
 
-while True:
-    print("Choisissez parmi les 5 options suivantes :\n"
-          "1_ Ajouter un ou plusieurs éléments à la liste\n"
-          "2_ Retirer un ou plusieurs éléments à la liste\n"
-          "3_ Afficher la liste\n"
-          "4_ Vider la liste\n"
-          "5_ Quitter le programme\n")
+    with open(chemin_json, 'w') as file:
+        json.dump("", file, indent=4)
+else:
+    grocery_list = json.load(open(chemin_json))
 
-    user_choice = input("Veuillez saisir votre réponse : ")
+grocery_list = list_main_ui(grocery_list)
 
-    if user_choice == "1":
-        grocery_list = list_append(grocery_list, input("Quel(s) ingrédient(s) souhaitez-vous ajouter ?\n"))
-    elif user_choice == "2":
-        list_remove(grocery_list, input("Quel(s) ingrédient(s) souhaitez-vous retirer ?\n"))
-    elif user_choice == "3":
-        list_print(grocery_list)
-    elif user_choice == "4":
-        list_empty(grocery_list)
-    elif user_choice == "5":
-        break
-    else:
-        continue
-
-    print("\n")
+with open(chemin_json, 'w') as file:
+    json.dump(grocery_list, file, indent=4)
